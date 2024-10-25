091c5e2b7738e539231b06c236d2d952289756bd
hanshuobo <1432101420@qq.com>
2024-10-25 09:31:49 +0800

    feat: react-redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useDispatch, useSelector } from 'react-redux'
+//导入actionCreater
+import { increment, decrement, addToNum } from './store/modules/counterStore'
+import { fetchChannelList } from './store/modules/channelStore'
+import { useEffect } from 'react'
 
 function App() {
+  const { count } = useSelector(state => state.counter)
+  const { channelList } = useSelector(store => store.channel)
+  const dispatch = useDispatch()
+
+  //使用useEffect触发异步请求执行
+  useEffect(() => {
+    dispatch(fetchChannelList())
+  }, [dispatch])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      <span>{count}</span>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(addToNum(10))}>add to 10</button>
+      <button onClick={() => dispatch(addToNum(20))}>add to 20</button>
+
+      <ul>
+        {channelList.map(item => <li key={item.id}>{item.name}</li>)}
+      </ul>
     </div>
   );
 }
